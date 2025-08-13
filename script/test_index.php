96b21ffe4d01f95f737a7d145bcb86f3e7fa2b38
Ole Oldhoj <ool@haandvaerker.dk>
2025-08-13 18:06:25 +0800

    update

--- script/test_index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__ . '/../public_html/index.php';
-$result = transcribe_path('nonexistent_path');
-if ($result !== 'Error: path not found') {
-    fwrite(STDERR, "Unexpected output: $result\n");
-    exit(1);
-}
-echo "OK\n";
-?>
