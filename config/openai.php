5186f0a3c5f0e910b8378e4ba8fbcc382e073ffb
Ole Oldhoj <ool@haandvaerker.dk>
2025-08-28 17:52:29 +0800

    update

--- config/openai.php
+++ config/openai.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'api_key'     => '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',
+    'api_key'     => env('OPENAI_API_KEY',''),
     'audio_model' => env('OPENAI_AUDIO_MODEL', 'gpt-4o-transcribe'),
 ];
